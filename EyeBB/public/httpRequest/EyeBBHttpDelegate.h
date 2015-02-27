b5fab188dcaa811e1c13c35493dc43ff62be2134
EvanYanGitHub <jackhuang@jackhuangtekiMac-mini.local>
2015-02-27 10:57:41 +0800

    删除httpDel里的错误
     删除httpDel里的错误

--- EyeBB/public/httpRequest/EyeBBHttpDelegate.h
+++ EyeBB/public/httpRequest/EyeBBHttpDelegate.h
@@ -9,13 +9,13 @@
 #import <Foundation/Foundation.h>
 @protocol EyeBBHttpDelegate<NSObject>
 
--(void)onReciveData:(ZCDHMessageResponse *) messageResponse with:(long)tag;
--(void)onError:(NSError *)error with:(long)tag;
-//-(void)startRequest;
-//-(void)endResponse;
--(void)endResponse:(BOOL)resultStatus;
--(void)startRequest:(NSString *)ViewControllerName;
--(void)setMyView:(NSString *)ViewControllerName;
-/**显示加载层*/
--(void)ShowView;
+//-(void)onReciveData:(ZCDHMessageResponse *) messageResponse with:(long)tag;
+//-(void)onError:(NSError *)error with:(long)tag;
+////-(void)startRequest;
+////-(void)endResponse;
+//-(void)endResponse:(BOOL)resultStatus;
+//-(void)startRequest:(NSString *)ViewControllerName;
+//-(void)setMyView:(NSString *)ViewControllerName;
+///**显示加载层*/
+//-(void)ShowView;
 @end
