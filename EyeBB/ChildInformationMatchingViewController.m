1215aa90c26db25cf74e607d267187d529a8b67b
leeway1208 <leeway1208@vip.qq.com>
2015-03-06 12:00:25 +0800

    doing child informationMatching view

--- EyeBB/ChildInformationMatchingViewController.m
+++ EyeBB/ChildInformationMatchingViewController.m
@@ -22,7 +22,7 @@
 
 /**kids kindergarten*/
 @property (nonatomic,strong) UILabel *kidsKindergartenLb;
-@property (nonatomic,strong) UIButton *KidsKindergartenBtn;
+@property (nonatomic,strong) UIButton *kidsKindergartenBtn;
 @property (nonatomic,strong) UIImageView *kidsKindergartenImg;
 
 /**search button*/
@@ -56,8 +56,32 @@
 
 -(void)viewDidDisappear:(BOOL)animated
 {
+    [_kidsNameTf removeFromSuperview];
+    [_kidsNameLb removeFromSuperview];
+    [_kidsNameImg removeFromSuperview];
     
+    [_kidsBirthdayBtn removeFromSuperview];
+    [_kidsBirthadyLb removeFromSuperview];
+    [_kidsBirthdayImg removeFromSuperview];
+    
+    [_kidsKindergartenLb removeFromSuperview];
+    [_kidsKindergartenBtn removeFromSuperview];
+    [_kidsKindergartenImg removeFromSuperview];
+    
+    [_searchBtn removeFromSuperview];
     [self.view removeFromSuperview];
+    [self setKidsNameTf:nil];
+    [self setKidsNameLb:nil];
+    [self setKidsNameImg:nil];
+    
+    [self setKidsBirthdayBtn:nil];
+    [self setKidsBirthadyLb:nil];
+    [self setKidsBirthdayImg:nil];
+    
+    [self setKidsKindergartenLb:nil];
+    [self setKidsKindergartenBtn:nil];
+    [self setKidsKindergartenImg:nil];
+    [self setSearchBtn:nil];
     
     [self setView:nil];
     [super viewDidDisappear:animated];
@@ -65,8 +89,12 @@
 
 -(void)loadWidget{
     
+    /*
+     *  search button
+     */
+    
     //登录按钮
-    _searchBtn =[[UIButton alloc]initWithFrame:CGRectMake((Drive_Wdith/2)-(Drive_Wdith/4), 200, (Drive_Wdith/2), Drive_Wdith/8)];
+    _searchBtn =[[UIButton alloc]initWithFrame:CGRectMake((Drive_Wdith/2)-(Drive_Wdith/4), 400, (Drive_Wdith/2), Drive_Wdith/8)];
     //设置按显示文字
     [_searchBtn setTitle:LOCALIZATION(@"btn_search_child") forState:UIControlStateNormal];
     [_searchBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
@@ -74,7 +102,7 @@
     //设置按钮背景颜色
     [_searchBtn setBackgroundColor:[UIColor colorWithRed:0.914 green:0.267 blue:0.235 alpha:1]];
     //设置按钮响应事件
-    [_searchBtn addTarget:self action:@selector(loginAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_searchBtn addTarget:self action:@selector(searchChildAction:) forControlEvents:UIControlEventTouchUpInside];
     //设置按钮是否圆角
     [_searchBtn.layer setMasksToBounds:YES];
     //圆角像素化
@@ -83,7 +111,8 @@
     [_searchBtn.layer setBorderColor:[UIColor colorWithRed:0.914 green:0.267 blue:0.235 alpha:1].CGColor];//边框颜色
     [self.view addSubview:_searchBtn];
     
-
+    
+    
     /**
      * kids name view
      */
@@ -110,28 +139,77 @@
     [kidsNameDivLb.layer setBorderColor:[UIColor colorWithRed:0.914 green:0.267 blue:0.235 alpha:1].CGColor];
     
     [self.view  addSubview:kidsNameDivLb];
-
-     /**title label*/
+    
+    /**title label*/
     _kidsNameLb = [[UILabel alloc]initWithFrame:CGRectMake(10, 90, self.view.frame.size.width-20, 20)];
     _kidsNameLb.text = LOCALIZATION(@"text_fill_kid_name");
     [_kidsNameLb setTextColor : [UIColor redColor]];
     [_kidsNameLb setFont: [UIFont systemFontOfSize:14]];
     [self.view  addSubview:_kidsNameLb];
-}
-
--(void)loadParameter{
     
-}
-
-
+    
+    /*
+     *  kids birthday
+     */
+    
+    _kidsBirthdayBtn=[[UIButton alloc] initWithFrame:self.view.bounds];
+    _kidsBirthdayBtn.frame = CGRectMake(-75, 190,self.view.frame.size.width , 40);
+    _kidsBirthdayBtn.contentVerticalAlignment=UIControlContentVerticalAlignmentCenter;//设置其输入内容竖直居中
+    [_kidsBirthdayBtn setTitle:LOCALIZATION(@"text_kid_birthday") forState:UIControlStateNormal];
+    [_kidsBirthdayBtn setTitleColor:[UIColor colorWithRed:0.725 green:0.725 blue:0.745 alpha:1]forState:UIControlStateNormal];
+    _kidsBirthdayBtn.titleLabel.font = [UIFont systemFontOfSize:17];
+    [_kidsBirthdayBtn addTarget:self action:@selector(kidsBirthdayAciton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_kidsBirthdayBtn];
+     _kidsNameImg =[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"login_name"]];
+     _kidsNameImg.frame = CGRectMake(10, 200, 20, 20);
+     [self.view addSubview:_kidsNameImg];
+     //    _kidsBirthdayBtn.leftView = _kidsNameImg;//设置输入框内左边的图标
+     //    _kidsBirthdayBtn.clearButtonMode=UITextFieldViewModeWhileEditing;//右侧删除按钮
+     //
+     //    //[_loginUserAccount becomeFirstResponder];
+     //    [self.view addSubview:_kidsNameTf];
+     //
+     //    /**Dividing line*/
+     //    UILabel * kidsNameDivLb=[[UILabel alloc]initWithFrame:CGRectMake(10, 145, self.view.frame.size.width-20, 1)];
+     //    [kidsNameDivLb.layer setBorderWidth:1.0]; //边框宽度
+     //    [kidsNameDivLb.layer setBorderColor:[UIColor colorWithRed:0.914 green:0.267 blue:0.235 alpha:1].CGColor];
+     //
+     //    [self.view  addSubview:kidsNameDivLb];
+     
+     /**title label*/
+     _kidsBirthadyLb = [[UILabel alloc]initWithFrame:CGRectMake(10, 175, self.view.frame.size.width-20, 20)];
+     _kidsBirthadyLb.text = LOCALIZATION(@"text_select_kid_birthday");
+     [_kidsBirthadyLb setTextColor : [UIColor redColor]];
+     [_kidsBirthadyLb setFont: [UIFont systemFontOfSize:14]];
+     [self.view  addSubview:_kidsBirthadyLb];
+     
+     
+     /**Dividing line*/
+     UILabel * kidsBirthdayDivLb=[[UILabel alloc]initWithFrame:CGRectMake(10, 225, self.view.frame.size.width-20, 1)];
+     [kidsBirthdayDivLb.layer setBorderWidth:1.0]; //边框宽度
+     [kidsBirthdayDivLb.layer setBorderColor:[UIColor colorWithRed:0.914 green:0.267 blue:0.235 alpha:1].CGColor];
+     
+     [self.view  addSubview:kidsBirthdayDivLb];
+     
+     }
+     
+     -(void)loadParameter{
+         
+     }
+     
+     
 #pragma - button action
-- (void)loginSelectLeftAction:(id)sender{
-    [[self navigationController] pushViewController:nil animated:YES];
-}
-
-
-@end
-
-
-
-
+     - (void)loginSelectLeftAction:(id)sender{
+         [[self navigationController] pushViewController:nil animated:YES];
+     }
+     
+     - (void)searchChildAction:(id)sender{
+         
+     }
+     
+     
+     @end
+     
+     
+     
+     
