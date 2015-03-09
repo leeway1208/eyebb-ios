8bbc5c6fac2a4d615aae4c6bf71ae7fd338f7938
leeway1208 <leeway1208@vip.qq.com>
2015-03-09 10:40:52 +0800

    doing new view

---
+++ EyeBB/KindergartenListViewController.h
@@ -0,0 +1,13 @@
+//
+//  KindergartenListViewController.h
+//  EyeBB
+//
+//  Created by liwei wang on 6/3/15.
+//  Copyright (c) 2015 EyeBB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KindergartenListViewController : UIViewController
+
+@end
---
+++ EyeBB/KindergartenListViewController.m
@@ -0,0 +1,13 @@
+//
+//  KindergartenListViewController.m
+//  EyeBB
+//
+//  Created by liwei wang on 6/3/15.
+//  Copyright (c) 2015 EyeBB. All rights reserved.
+//
+
+#import "KindergartenListViewController.h"
+
+@implementation KindergartenListViewController
+
+@end
